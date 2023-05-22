28d1f78c9448ece0a958fb2bfb831474af842013
ousemaa <ayro69tn@gmail.com.com>
2023-05-22 10:03:27 +0100

    first commit

---
+++ models/user.js
@@ -0,0 +1,14 @@
+const mongoose=require("mongoose");
+const schema=mongoose.Schema;
+
+const userSchema = new schema({
+    name:String,
+    lastname:String,
+    age:Number,
+    email:{type:String, unique:true},
+    isAdmin:{type:Boolean, default:false},
+    
+  });
+
+  const User = mongoose.model('User', userSchema);
+  module.exports = User;
